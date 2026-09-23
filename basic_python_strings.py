63ffab6d51900de1a8b8842b1e76eeeb00feb620
James Mahan <jcmahan@gmail.com>
2026-09-23 12:01:07 -0700

    'string manipulation practice'

---
+++ basic_python_strings.py
@@ -0,0 +1,16 @@
+a = '1234'
+i = int(a)
+print(a)
+print(type(a))
+print(i)
+print(type(i))
+print(float(a))
+print(a[0])
+print(a[-1])
+
+my_string = 'Hello James'
+print(my_string[0:5])
+print(my_string[-1:-2])
+
+nested_list = [1,2,3,4,['Hello', 'James']]
+print(nested_list[-1][-1])
